3896a9dddd1ea5f1ed8ac08331c2085f20abf421
Kingsley <kingsleyterwase5@gmail.com>
2025-07-11 11:38:23 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: process.env.VITE_BASE_URL || "/Rem-church",
+});
